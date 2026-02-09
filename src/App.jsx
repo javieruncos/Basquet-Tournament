e89f4ff1d6931c9cbe0d27ef3dbd980b9f0715fd
Javier Uncos <javiersrfc30@gmail.com>
2026-02-09 16:55:21 -0300

    estilos a la portada de detalle de partido

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter,Route, Routes } from "react-router-dom";
 import MenuNav from "./components/common/MenuNav";
 import Noticias from "./pages/noticias/Noticias";
 import Resultados from "./pages/resultados/Resultados";
+import DetalleResultado from "./pages/detalleResultado/DetalleResultado";
 
 function App() {
   
@@ -13,7 +14,8 @@ function App() {
          <MenuNav></MenuNav>
          <Routes>
           <Route path="/noticias" element={<Noticias></Noticias>}></Route>
-          <Route path="/" element={<Resultados></Resultados>}></Route>
+          <Route path="/resultados" element={<Resultados></Resultados>}></Route>
+          <Route path="/" element={<DetalleResultado></DetalleResultado>}></Route>
          </Routes>
       </div>
     </BrowserRouter>
---
+++ src/pages/detalleResultado/DetalleResultado.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PortadaDetalle from './components/PortadaDetalle';
+
+const DetalleResultado = () => {
+    return (
+        <div className='p-10'>
+          <PortadaDetalle></PortadaDetalle>
+        </div>
+    );
+};
+
+export default DetalleResultado;
