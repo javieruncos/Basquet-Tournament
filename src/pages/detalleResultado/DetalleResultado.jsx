09c5d7cd22479525418b672a77ec55b98d1ffb84
Javier Uncos <javiersrfc30@gmail.com>
2026-02-10 12:32:12 -0300

    estilos a la seccion de resultados por cuarto de partido

--- src/pages/detalleResultado/DetalleResultado.jsx
+++ src/pages/detalleResultado/DetalleResultado.jsx
@@ -3,8 +3,50 @@ import PortadaDetalle from './components/PortadaDetalle';
 
 const DetalleResultado = () => {
     return (
-        <div className='pt-5 px-3 md:p-10'>
+        <div className='pt-5 px-3 md:p-10 px'>
           <PortadaDetalle></PortadaDetalle>
+          <div className='grid grid-cols-2 md:grid-cols-4 mt-10 gap-5 md:gap-3 lg:gap-10'>
+            <div className='h-35 lg:h-40 w-full bg-[#171717] rounded-md'>
+                <div className='p-4 text-center lg:text-left'>
+                    <span className='text-gray-400 numberFonts font-bold'>1 cuarto</span>
+                </div>
+                <div className='flex justify-center items-center gap-3 text-3xl lg:text-6xl font-bold numberFonts'>
+                   <span className='text-amber-300'>22</span>
+                   <span>-</span>
+                   <span>18</span>
+                </div>
+            </div>
+            <div className='h-35 lg:h-40 w-full bg-[#171717] rounded-md'>
+                <div className='p-4 text-center lg:text-left'>
+                    <span className='text-gray-400 numberFonts font-bold'>2 cuarto</span>
+                </div>
+                <div className='flex justify-center items-center gap-3 text-3xl lg:text-6xl font-bold numberFonts'>
+                   <span className='text-amber-300'>22</span>
+                   <span>-</span>
+                   <span>18</span>
+                </div>
+            </div>
+            <div className='h-35 lg:h-40 w-full bg-[#171717] rounded-md'>
+                <div className='p-4 text-center lg:text-left'>
+                    <span className='text-gray-400 numberFonts font-bold'>3 cuarto</span>
+                </div>
+                <div className='flex justify-center items-center gap-3 text-3xl lg:text-6xl font-bold numberFonts'>
+                   <span className='text-amber-300'>22</span>
+                   <span>-</span>
+                   <span>18</span>
+                </div>
+            </div>
+            <div className='h-35 lg:h-40 w-full bg-[#171717] rounded-md'>
+                <div className='p-4 text-center lg:text-left'>
+                    <span className='text-gray-400 numberFonts font-bold'>4 cuarto</span>
+                </div>
+                <div className='flex justify-center items-center gap-3 text-3xl lg:text-6xl font-bold numberFonts'>
+                   <span className='text-amber-300'>22</span>
+                   <span>-</span>
+                   <span>18</span>
+                </div>
+            </div>     
+          </div>
         </div>
     );
 };
--- src/pages/detalleResultado/components/PortadaDetalle.jsx
+++ src/pages/detalleResultado/components/PortadaDetalle.jsx
@@ -1,52 +1,59 @@
-import portada from  "../../../assets/images/portadaResults2.png"
+import portada from "../../../assets/images/stadium.png";
 
 const PortadaDetalle = () => {
   return (
     <div className="min-h-60 md:h-90 lg:h-120 w-full rounded-md border-gray-400/30 border bg-[#171717] relative flex flex-col">
-      <img src={portada} alt="cancha"
-      className="absolute inset-0 w-full h-full object-cover overflow-hidden rounded-md"
+      <img
+        src={portada}
+        className="absolute inset-0 w-full h-full object-cover overflow-hidden rounded-md"
       />
-      <div className="relative z-10 bg-black/60 w-full h-full overflow-hidden rounded-md flex flex-col">
-      <div className="py-5 px-4 md:px-14">
-        <div className="flex gap-4 items-center justify-center pt-3">
-          <div className="flex gap-3 md:gap-5 border-b border-amber-400 pb-3">
-          <span className="px-3 py-1 bg-yellow-300 rounded-md text-black text-sm md:text-base">
-            Fecha 5{" "}
-          </span>
-          <span className="font-bold text-lg md:text-2xl numberFonts text-white">31/12/2022</span>
+      <div className="relative z-10 bg-black/85 min-h-60 h-full overflow-hidden rounded-md flex flex-col">
+        <div className="py-5 px-4 md:px-14">
+          <div className="flex gap-4 items-center justify-center pt-3">
+            <div className="flex gap-3 md:gap-5 border-b border-amber-400 pb-3">
+              <span className="px-3 py-1 bg-yellow-300 rounded-md text-black text-sm md:text-base">
+                Fecha 5{" "}
+              </span>
+              <span className="font-bold text-md md:text-2xl numberFonts text-white">
+                31/12/2022
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="grid grid-cols-3 items-center grow px-2 md:px-4 pb-6 md:pb-0 gap-5 md:gap-10 ">
-        <div className="flex flex-col items-center gap-2 md:gap-4  text-center">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Escudo_del_Club_Estaci%C3%B3n_Experimental_Tucum%C3%A1n.svg"
-            alt="Local"
-            className="h-16 w-16 md:h-32 md:w-32 lg:h-52 lg:w-52 object-contain"
-          />
-          <h2 className="text-sm md:text-2xl lg:text-4xl font-bold text-white uppercase">
+        <div className="grid grid-cols-3 items-center grow px-2 md:px-4 pb-6 md:pb-0 gap-5 md:gap-10 ">
+          <div className="flex flex-col items-center gap-2 md:gap-4  text-center">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Escudo_del_Club_Estaci%C3%B3n_Experimental_Tucum%C3%A1n.svg"
+              alt="Local"
+              className="h-16 w-16 md:h-32 md:w-32 lg:h-52 lg:w-52 object-contain"
+            />
+            <h2 className="text-sm md:text-2xl lg:text-4xl font-bold text-white uppercase">
               {"atle. tucuman".split(" ").slice(0, 2).join(" ")}
-          </h2>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <div className="flex gap-2 md:gap-4 text-4xl md:text-7xl lg:text-9xl font-bold text-white numberFonts">
-            <span>88</span>
-            <span className="text-amber-300">-</span>
-            <span>74</span>
+            </h2>
           </div>
-          <span className="text-gray-400 mt-2 uppercase tracking-widest md:tracking-[0.5em] text-xs md:text-base numberFonts text-center">Finalizado</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 md:gap-4 text-center">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Escudo_del_Club_Estaci%C3%B3n_Experimental_Tucum%C3%A1n.svg"
-            alt="Visitante"
-            className="h-16 w-16 md:h-32 md:w-32 lg:h-52 lg:w-52 object-contain"
-          />
-          <h2 className="text-sm md:text-2xl lg:text-4xl font-bold text-white uppercase ">
+          <div className="flex flex-col items-center justify-center">
+            <div className="flex gap-2 md:gap-4 text-4xl md:text-7xl lg:text-9xl font-bold text-white numberFonts">
+              <span>88</span>
+              <span className="text-amber-300">-</span>
+              <span>74</span>
+            </div>
+            <span className="text-gray-400 mt-2 uppercase tracking-widest md:tracking-[0.5em] text-xs md:text-base numberFonts text-center">
+              Finalizado
+            </span>
+          </div>
+          <div className="flex flex-col items-center gap-2 md:gap-4 text-center">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Escudo_del_Club_Estaci%C3%B3n_Experimental_Tucum%C3%A1n.svg"
+              alt="Visitante"
+              className="h-16 w-16 md:h-32 md:w-32 lg:h-52 lg:w-52 object-contain"
+            />
+            <h2 className="text-sm md:text-2xl lg:text-4xl font-bold text-white uppercase ">
               {"Est. Experimental".split(" ").slice(0, 2).join(" ")}
-          </h2>
+            </h2>
+          </div>
         </div>
       </div>
+      <div>
       </div>
     </div>
   );
