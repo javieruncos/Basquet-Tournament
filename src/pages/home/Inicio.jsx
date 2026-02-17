04f140d4198c8e4d6ddd48183db5739041096a48
Javier Uncos <javiersrfc30@gmail.com>
2026-02-16 22:37:24 -0300

    estilos de fuente para numeros agregado

--- src/pages/home/Inicio.jsx
+++ src/pages/home/Inicio.jsx
@@ -7,7 +7,7 @@ export const Inicio = () => {
   return (
     <div className="bg-[#0b0b0b] text-white">
       <PortadaInicio />
-      
+
       <section className="py-20 px-4 sm:px-6 lg:px-8 ">
         <div className="max-w-7xl mx-auto">
           <div className="mb-12 flex justify-between items-center">
@@ -41,10 +41,10 @@ export const Inicio = () => {
               "
               >
                 <div className="flex justify-between mb-6">
-                  <span className="text-amber-300 text-xs font-bold uppercase tracking-widest">
+                  <span className="text-amber-300 text-xs font-bold uppercase tracking-widest numberFonts">
                     25 JUN 2026
                   </span>
-                  <span className="text-gray-400 text-sm">20:00 HS</span>
+                  <span className="text-gray-400 text-sm numberFonts">20:00 HS</span>
                 </div>
 
                 <div className="flex flex-col gap-4">
@@ -151,7 +151,7 @@ export const Inicio = () => {
                   </div>
 
                   <div className="absolute bottom-16 left-6 text-white">
-                    <p className="text-4xl font-extrabold">1</p>
+                    <p className="text-4xl font-extrabold numberFonts">1</p>
                     <p className="text-xs uppercase text-gray-300">Place</p>
                   </div>
                   <div className="absolute bottom-6 left-6 right-6 text-white">
@@ -167,32 +167,32 @@ export const Inicio = () => {
                 <div  className=" to-black p-6 border border-white/10">
                   <div className="grid grid-cols-3 text-center text-white divide-x divide-amber-300">
                     <div className="pb-6">
-                      <p className="text-2xl font-extrabold">27</p>
+                      <p className="text-2xl font-extrabold numberFonts">27</p>
                       <p className="text-xs uppercase text-amber-100/70">Minutes</p>
                     </div>
 
                     <div className="pb-6">
-                      <p className="text-2xl font-extrabold">21</p>
+                      <p className="text-2xl font-extrabold numberFonts">21</p>
                       <p className="text-xs uppercase text-amber-100/70">Points</p>
                     </div>
 
                     <div className="pb-6 border-r-0!">
-                      <p className="text-2xl font-extrabold">52%</p>
+                      <p className="text-2xl font-extrabold numberFonts">52%</p>
                       <p className="text-xs uppercase text-amber-100/70">FG%</p>
                     </div>
 
                     <div className="pt-6">
-                      <p className="text-2xl font-extrabold">13</p>
+                      <p className="text-2xl font-extrabold numberFonts">13</p>
                       <p className="text-xs uppercase text-amber-100/70">Rebounds</p>
                     </div>
 
                     <div className="pt-6">
-                      <p className="text-2xl font-extrabold">3.7</p>
+                      <p className="text-2xl font-extrabold numberFonts">3.7</p>
                       <p className="text-xs uppercase text-amber-100/70">Assists</p>
                     </div>
 
                     <div className="pt-6">
-                      <p className="text-2xl font-extrabold">0.7</p>
+                      <p className="text-2xl font-extrabold numberFonts">0.7</p>
                       <p className="text-xs uppercase text-amber-100/70">Steals</p>
                     </div>
                   </div>
@@ -241,7 +241,7 @@ export const Inicio = () => {
                     key={pos}
                     className="border-b border-white/5 hover:bg-white/10 transition"
                   >
-                    <td className="p-3 md:p-5 text-center font-bold text-base md:text-lg">{pos}</td>
+                    <td className="p-3 md:p-5 text-center font-bold text-base md:text-lg numberFonts">{pos}</td>
 
                     <td className="p-3 md:p-5">
                       <div className="flex items-center justify-center gap-3">
@@ -255,11 +255,11 @@ export const Inicio = () => {
                       </div>
                     </td>
 
-                    <td className="p-3 md:p-5 text-center text-sm md:text-base">10</td>
-                    <td className="p-3 md:p-5 text-center text-green-400 text-sm md:text-base">8</td>
-                    <td className="p-3 md:p-5 text-center text-red-400 text-sm md:text-base">2</td>
+                    <td className="p-3 md:p-5 text-center text-sm md:text-base numberFonts">10</td>
+                    <td className="p-3 md:p-5 text-center text-green-400 text-sm md:text-base numberFonts">8</td>
+                    <td className="p-3 md:p-5 text-center text-red-400 text-sm md:text-base numberFonts">2</td>
 
-                    <td className="p-3 md:p-5 text-center font-bold text-amber-300 text-base md:text-lg">
+                    <td className="p-3 md:p-5 text-center font-bold text-amber-300 text-base md:text-lg numberFonts">
                       18
                     </td>
                   </tr>
