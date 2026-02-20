260559fc4e06da6209c530d293aa81585b67ee51
Javier Uncos <javiersrfc30@gmail.com>
2026-02-20 00:35:29 -0300

    funcion para crear noticia

--- src/services/NewsService.js
+++ src/services/NewsService.js
@@ -13,3 +13,14 @@ export const getNoticias = async () => {
     throw new Error("No se pudieron cargar las noticias");
   }
 };
+
+
+export const crearNoticia = async (formData) => {
+  try {
+    const response = await api.post("/", formData);
+    return response.data;
+  } catch (error) {
+    console.error("Error al crear noticia:", error.message);
+    throw new Error("No se pudo crear la noticia");
+  }
+}
